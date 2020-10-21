aadb435c802acd08145e8ba463dfdac132391ca8
Thanh <thanh@thanh.truong>
2020-10-21 18:00:49 +0700

    add ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -36,7 +36,7 @@ class Ability
       end
 
       if highest_role.get_permission("can_manage_users")
-        can [:index, :edit_user, :promote, :demote, :ban_user, :unban_user,
+        can [:index, :manual_create_user, :edit_user, :promote, :demote, :ban_user, :unban_user,
              :approve, :invite, :reset, :undelete, :merge_user], :admin
       end
 
